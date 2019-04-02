b0e83fc90c9533a28e3ac807f83e831b7473592a
Ankit Kumar <ankit_sony3@hotmail.com>
2019-04-02 17:18:15 +0530

    adde create parking lot function

--- models/parking_lot.go
+++ models/parking_lot.go
@@ -1,5 +1,7 @@
 package models
 
+import "errors"
+
 // ParkingLot :
 type ParkingLot struct {
 	lotID         string
@@ -8,4 +10,16 @@ type ParkingLot struct {
 	size          int
 	nextSpot      int
 	hasEmptySpace bool
+}
+
+// CreateParkingLot : this function reurns a parking lot with the provided size of parking spots
+// by default paking spot size will be "SM" (ASSUMPTION)
+// by default level will be "0" (ASSUMPTION)
+func CreateParkingLot(lotID, address string, size int) (*ParkingLot, error) {
+	if size <= 0 {
+		return nil, errors.New("Invalid Parking Spot Aize")
+	}
+
+	return nil, nil
+
 }

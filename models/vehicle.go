4ed21a7279f9990117a7287887f3cf9e4aefb8ed
Ankit Kumar <ankit_sony3@hotmail.com>
2019-04-02 16:04:26 +0530

    added vehicle model

---
+++ models/vehicle.go
@@ -0,0 +1,25 @@
+package models
+
+// Vehicle : Vehicle type to create and store a vehicle in parking spot
+type Vehicle struct {
+	color     string // Color of the vehicle
+	vehicleNo string // Registration no of vehicle
+
+	// TODO: change type of size, string to enum/constant
+	size string // stores the vehicle size
+}
+
+// GetColor : returns color of the vehicle
+func (v *Vehicle) GetColor() string {
+	return v.color
+}
+
+// GetVehicleNo : returns vehicle no
+func (v *Vehicle) GetVehicleNo() string {
+	return v.vehicleNo
+}
+
+// GetSize : returns size of the vehicle
+func (v *Vehicle) GetSize() string {
+	return v.size
+}
